609c727338ada82fcc7fbde3f28aab6a3a622b09
Rodrigo Bejar <rodrigozx23@gmail.com>
2024-04-29 02:38:12 -0500

    Datepicker new version.
    Fixes for datepicker range

---
+++ src/components/DateRangePicker/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const DateRangePicker = ({ handleSearchDate }) => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const handleDateChange = dates => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+    handleSearchDate(start, end);
+  };
+
+  return (
+    <div className="input-group">
+      <DatePicker
+        selected={startDate}
+        onChange={handleDateChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
+        dateFormat="dd/MM/yyyy"
+        className="form-control"
+        placeholderText="Select Date Range"
+      />
+    </div>
+  );
+};
+
+export {DateRangePicker};
