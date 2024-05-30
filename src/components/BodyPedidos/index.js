384539cb5555fcb4ad6b640ae91692ea43dec783
Eduardo <eduardo.orihuela@globant.com>
2024-05-30 16:01:20 -0500

    Last changes pedido

--- src/components/BodyPedidos/index.js
+++ src/components/BodyPedidos/index.js
@@ -8,65 +8,67 @@ function BodyPedidos({
   openCrearPedidoDetails,
   openUpdPedidoDetails,
   openReportePedidos,
-  pedidoData, 
+  pedidoData,
   setPedidoData
 }) {
   const [loading, setLoading] = useState(true);
-	useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       try {
-      const data = await fetchPedidos(); // Assuming fetchProdutos correctly fetches the data
-      if (Array.isArray(data)) {
-        // Check if the response is an array
-        const modifiedData = data.map(item => {
-          // Parse the fecha_creacion string into a Date object
-          const fechaCreacionDate = new Date(item.fecha_creacion);
+        const data = await fetchPedidos(); // Assuming fetchProdutos correctly fetches the data
+        if (Array.isArray(data)) {
+          // Check if the response is an array
+          const modifiedData = data.map(item => {
+            // Parse the fecha_creacion string into a Date object
+            const fechaCreacionDate = new Date(item.fecha_creacion);
 
-          // Format day and month as "dd/mm"
-          const formattedDate = fechaCreacionDate.toLocaleDateString('en-GB', {
-            day: '2-digit',
-            month: '2-digit',timeZone: 'UTC', // Set the time zone to UTC
+            // Format day and month as "dd/mm"
+            const formattedDate = fechaCreacionDate.toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: '2-digit', timeZone: 'UTC', // Set the time zone to UTC
+            });
+
+            const formattedTime = fechaCreacionDate.toLocaleTimeString('en-GB', {
+              hour: '2-digit',
+              minute: '2-digit', timeZone: 'UTC', // Set the time zone to UTC
+            });
+
+            return {
+              id: item.id,
+              cliente: item.cliente,
+              mesa: item.mesa,
+              estadoPedido: item.estado_pedido,
+              fecha: formattedDate,
+              hora: formattedTime,
+              total: item.total,
+              fecha_creacion: item.fecha_creacion
+            };
           });
-
-          const formattedTime = fechaCreacionDate.toLocaleTimeString('en-GB', {
-            hour: '2-digit',
-            minute: '2-digit',timeZone: 'UTC', // Set the time zone to UTC
-          });
-
-          return {
-            id: item.id,
-            cliente: item.cliente,
-            mesa: item.mesa,
-            estadoPedido: item.estado_pedido,
-            fecha: formattedDate,
-            hora: formattedTime,
-            total: item.total,
-            fecha_creacion : item.fecha_creacion
-          };
-        });
-        setPedidoData(modifiedData);        
-        setLoading(false)
-      } else {
-        console.error('Error: Data received from the API is not an array.');
-      }
+          setPedidoData(modifiedData);
+          setLoading(false)
+        } else {
+          console.error('Error: Data received from the API is not an array.');
+        }
       } catch (error) {
-      console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
       }
     };
     fetchData();
   }, [setPedidoData]);
 
-  const PedidoButton = ({ cliente, mesa, fecha, hora, total, onClick  }) => {
+  const PedidoButton = ({ cliente, mesa, fecha, hora, total, onClick }) => {
     return (
       <button className='ordersButton' onClick={onClick}>
-         <p><span className='left'>{fecha}</span> <span className='right'><b>Total: {total} soles</b></span></p>
-         <br />
-         <p style={{ margin: '0px 0px 0px 8px', textAlign: 'left' }}>Cliente: {cliente}
-         <br />
-         Mesa: {mesa}</p>
-         <br />
-         <span className='right up'>{ hora }</span>
-         <p className='ordersButBotton'><span className='left'><b></b></span></p>
+        <p><span className='left'><b>{fecha}</b></span> <span className='right'><b>Total:</b> {total} soles</span></p>
+        <br />
+        <p style={{ margin: '0px 0px 0px 8px', textAlign: 'left' }}>
+          <b>Cliente:</b> {cliente}
+          <br />
+          <b>Mesa:</b> {mesa}
+        </p>
+        <br />
+        <span className='right up'>{hora}</span>
+        <p className='ordersButBotton'><span className='left'><b></b></span></p>
       </button>
     );
   };
@@ -83,21 +85,21 @@ function BodyPedidos({
         <h1><b>Pedidos Activos</b></h1>
       </div>
       <div className="col-md-12 mt-3">
-      <br />
-      <br />
-      {loading ? (
+        <br />
+        <br />
+        {loading ? (
           <p>Loading...</p>
-        ) : (          
+        ) : (
           activeOrders.map(order => (
-            <PedidoButton key={order.id} cliente={order.cliente} mesa={order.mesa} fecha={order.fecha} hora = {order.hora} total = {order.total}
-            onClick={() => {
-              openUpdPedidoDetails(order.id, order.cliente, order.mesa, order.total)
-            }} />
-          ))          
+            <PedidoButton key={order.id} cliente={order.cliente} mesa={order.mesa} fecha={order.fecha} hora={order.hora} total={order.total}
+              onClick={() => {
+                openUpdPedidoDetails(order.id, order.cliente, order.mesa, order.total)
+              }} />
+          ))
         )}
       </div>
     </div>
   );
 }
 
-export {BodyPedidos} ;
+export { BodyPedidos };
